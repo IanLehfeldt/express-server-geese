c570e745393308cbd7cde20c830279cc441a7952
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-08-02 16:47:47 -0500

    Added logic to the new geese function on app.js

--- app.js
+++ app.js
@@ -27,22 +27,35 @@ app.use(express.static('public'));
 //if we have a json body, if we didnt have true, it would flatten the body
 //true returns a tree structure.
 //Anytime you want to post anything, you need body parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //app checks if we're searching for a /geese url, its gonna send back the geese array
-app.get('/geese', function(req, res){
+app.get('/geese', function (req, res) {
     res.send(geese);
 });
 
-app.post('/geese', function(req, res){
-    console.log(req.body);
-    geese.push(req.body);
-    //the data from the $.ajax function from the newGeese button is returned in the
-    //req.body
-    //we need to install body-parser to the node so we can manipulate the data we received
-    res.sendStatus(201);
+app.post('/geese', function (req, res) {
+    for (i = 0; i < geese.length; i++) {
+        if (req.body.name === geese[i].name) {
+            console.log('ERRORR ERROR ERROR');
+            req.body.remove();
+        }
+    }
+
+    if (req.body.name === '') {
+        console.log('ErrorERROR EROROR');
+        res.sendStatus(400);
+    } else {
+        console.log(req.body);
+        req.body.dateAdded = 
+        geese.push(req.body);
+        //the data from the $.ajax function from the newGeese button is returned in the
+        //req.body
+        //we need to install body-parser to the node so we can manipulate the data we received
+        res.sendStatus(201);
+    }
 });
 
-app.listen(port, function(){
-    console.log('Listening to port:', port); 
+app.listen(port, function () {
+    console.log('Listening to port:', port);
 });
